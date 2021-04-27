d28e79f48bce51e9bc14ea3c35c5d26580dbdfa2
Mathis Mensing <matmen@dreadful.tech>
2021-04-27 23:39:22 +0200

    don't remove cname

--- generate.sh
+++ generate.sh
@@ -7,8 +7,10 @@ node convert.js
 
 jsdoc --configure .jsdoc.json --verbose
 
+mv docs/CNAME CNAME
 rm -r docs/*
 mv public/* docs
 rm -r public
+mv CNAME docs/CNAME
 
 cp ./favicon.ico ./public/favicon.ico
